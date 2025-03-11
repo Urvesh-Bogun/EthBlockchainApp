4731290d2f650123909cddc81f56249faadad12b
urvesh <urveshbogun@gmail.com>
2025-03-11 13:36:03 +0000

    Listed starting task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-contract TodoList{
+contract TodoList {
     uint public taskCount = 0;
 
     struct Task {
@@ -12,13 +12,12 @@ contract TodoList{
     mapping(uint => Task) public tasks;
 
     constructor() public {
-        createTask("Initialise");
+        taskCount = 1;
+        tasks[1] = Task(1, "Initialise", false);
     }
 
     function createTask(string memory _content) public {
-        taskCount ++;
-        tasks[taskCount] =Task(taskCount, _content, false);
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
     }
-
 }
-
